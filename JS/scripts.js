1c33a399710bbc724de2faa9509d58299153b216
DarrenDavis875 <59669452+DarrenDavis875@users.noreply.github.com>
2020-03-23 11:16:07 -0500

    working on the array

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,32 +1,41 @@
-var repository = [
-    {
-        name: 'Ivysour',
-        height:3,
-        type: ['grass', 'normal']
 
-    },
-    {
-        name: 'Blastoise',
-        height:4,
-        type: ['normal','grass']
-    },
-    {
-        name:'Pidgeot',
-        height:5,
-        type: ['poison','grass']
+
+
+
+
+ var pokemonRepository = (function(){
+      var repository =  [
+        {
+            name: 'Ivysour',
+            height:3,
+            type: ['grass', 'normal']
+    
+        },
+        {
+            name: 'Blastoise',
+            height:4,
+            type: ['normal','grass']
+        },
+        {
+            name:'Pidgeot',
+            height:5,
+            type: ['poison','grass']
+        }
+    
+    ];
+    function add(pokemon){
+        repository.push(pokemon);
     }
+    function getAll(){
+        return repository;
+    }
+    return {
+        add: add
+        getAll: getAll
+    };
+  })(); 
 
-];
-
-for(var i = 0; i < repository.length; i++){
-if (repository[i].height > 4) {
-    document.write(repository[i].name + ' (Height: ' + repository[i].height + ') Wow, thats big!' + '<br>');
-} else {
-     document.write(repository[i].name + ' (Height: ' + repository[i].height + ')' + '<br>');
-}
-
-}
-
-repository.forEach(function(property){
-    document.write (property.name + ' (Height: ', property.height + ')' + '<br>' )    
-}); 
+  pokemonRepository.getAll().forEach(function(pokemon){
+    document.write (pokemonRepository.add.name + ' (Height: ', pokemonRepository.add.height + ')' + '<br>' ) 
+});
+    
