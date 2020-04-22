4a8625b0a1da0d8a5f337f97d3e8b8e57db4462b
DarrenDavis875 <59669452+DarrenDavis875@users.noreply.github.com>
2020-04-22 08:08:30 -0500

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,7 @@
 var pokemonRepository = (function () {
     var repository = [];
     var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-    var $modalContainer = $('.modal-container');
+    var $modalContainer = $('#modal-container');
     function add(pokemon) {
       repository.push(pokemon);
     }
